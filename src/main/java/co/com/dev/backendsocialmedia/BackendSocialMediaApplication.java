80b4eb4df26fb4bcdc16ea829d420d76dc80036b
Miguel Andres Cortes Reyes <andresabyac@gmail.com>
2022-07-12 20:47:19 -0500

    init project backend

---
+++ src/main/java/co/com/dev/backendsocialmedia/BackendSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package co.com.dev.backendsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/dev/backendsocialmedia/BackendSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.dev.backendsocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
